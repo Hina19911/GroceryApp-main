18ca56e52a7a301a33b6ab8cd1c674b4c4c6d6b9
Hina19911 <Hina.Hina@triosstudent.com>
2025-02-02 21:53:06 -0500

    changed o to 0

--- GroceryAppTut/ContentView.swift
+++ GroceryAppTut/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack(spacing: 50) {
-                Image("avocado")
+                Image("avocad0")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 300, height: 300)
